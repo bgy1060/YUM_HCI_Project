ceb6500756379ff3e1c3e968ac0f0d0814c71dfb
박지연 <bgy1060@naver.com>
2020-05-29 14:46:01 +0900

    Updates

---
+++ precache-manifest.beaeb3f19ac62d0ad33dd91c05f28d60.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08c8576c391454fc9fdb28b8bbd1460",
+    "url": "/YUM_HCI_Project/index.html"
+  },
+  {
+    "revision": "f1657018b27893353f9f",
+    "url": "/YUM_HCI_Project/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b0c9317ba9055438fc6e",
+    "url": "/YUM_HCI_Project/static/css/main.2fe438c7.chunk.css"
+  },
+  {
+    "revision": "f1657018b27893353f9f",
+    "url": "/YUM_HCI_Project/static/js/2.74ca321a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/YUM_HCI_Project/static/js/2.74ca321a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c9317ba9055438fc6e",
+    "url": "/YUM_HCI_Project/static/js/main.c2b42898.chunk.js"
+  },
+  {
+    "revision": "2d92d0b76f47dd54a2e0",
+    "url": "/YUM_HCI_Project/static/js/runtime-main.e11cfe8f.js"
+  },
+  {
+    "revision": "3dbc4b103e3aef5b6703ff09b5a1cba9",
+    "url": "/YUM_HCI_Project/static/media/like.3dbc4b10.png"
+  },
+  {
+    "revision": "9b44bbad0a4133ddadbdba46a74d566c",
+    "url": "/YUM_HCI_Project/static/media/post1.9b44bbad.jpg"
+  },
+  {
+    "revision": "411982f8bbbabb6fead108404d093717",
+    "url": "/YUM_HCI_Project/static/media/post2.411982f8.jpg"
+  },
+  {
+    "revision": "e674f0835444d9fcd08c9123fe8c37cb",
+    "url": "/YUM_HCI_Project/static/media/post3.e674f083.jpg"
+  },
+  {
+    "revision": "dd1c23d52b50dac465b94b3b411bde89",
+    "url": "/YUM_HCI_Project/static/media/post4.dd1c23d5.jpg"
+  },
+  {
+    "revision": "ec53436214c18c645e7e0bce17dc9482",
+    "url": "/YUM_HCI_Project/static/media/post5.ec534362.jpg"
+  },
+  {
+    "revision": "d9f42e925131cd45168afa669fc5915e",
+    "url": "/YUM_HCI_Project/static/media/post6.d9f42e92.jpg"
+  },
+  {
+    "revision": "80b4fee4037c0c34fd2855f1c3ca0d5e",
+    "url": "/YUM_HCI_Project/static/media/share.80b4fee4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b85ca79d6ec4929051b06294f636027.js"
+  "/YUM_HCI_Project/precache-manifest.beaeb3f19ac62d0ad33dd91c05f28d60.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/YUM_HCI_Project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
