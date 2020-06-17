d885fd53fa744c245759d402c3141acb53cf91e7
uiuiui629 <uiuiuidrive@gmail.com>
2020-06-17 15:37:26 +0900

    modal 수정

--- src/components/MenuTemplate.js
+++ src/components/MenuTemplate.js
@@ -2,18 +2,16 @@ import React from 'react';
 import './MenuTemplate.css';
 import logo from './yum_logo.png';
 import { Link } from 'react-router-dom';
-import { Popover, OverlayTrigger } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+import { useState } from 'react';
 
-const popover = (
-  <Popover id = "popover-basic">
-    <Popover.Content className = "msg">
-      If you add the "tag" <br/>starting <strong>"#YUM_"</strong> <br/>to the blog post,<br/>
-      the post is posted <br/>on YUM hompage<br/> to promote it.
-    </Popover.Content>
-  </Popover>
-);
 
 const MenuTemplate = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div>
@@ -35,7 +33,7 @@ const MenuTemplate = () => {
                 <Link to='/YUM_HCI_Project/wood/' >
                     <li>#WOOD</li>
                 </Link>
-                
+
                 <Link to='/YUM_HCI_Project/blue/' >
                     <li>#BLUE</li>
                 </Link>
@@ -47,18 +45,26 @@ const MenuTemplate = () => {
                 <Link to='/YUM_HCI_Project/vintage/' >
                     <li>#VINTAGE</li>
                 </Link>
-                
+
                 <Link to='/YUM_HCI_Project/nordic/' >
                     <li>#NORDIC</li>
                 </Link>
-                
+
               </ul>
 
               <div className = "helpmsg">
                 <span className = "msg"> If you want to<br/>post your post?<br/> </span>
-                <OverlayTrigger trigger = "click" placement = "right-start" overlay = {popover}>
-                  <button variant = "success" className = "helpbtn" >CLICK</button>
-                </OverlayTrigger>
+
+                <button variant = "success" className = "helpbtn" onClick={handleShow}>CLICK</button>
+
+                <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>If you want to post your post?</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>If you add the "tag" starting "#YUM_" to the blog post,
+                  the post is posted on YUM hompage to promote it.</Modal.Body>
+                </Modal>
+
               </div>
 
             </div>
